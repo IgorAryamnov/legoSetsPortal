ba2a585743fa65ac2a64a268d433024e17dea9f3
IgorAryamnov <besenokhy@mail.ru>
2025-02-03 21:46:59 +0500

    uncomment logger

--- src/store/logger.js
+++ src/store/logger.js
@@ -1,12 +1,12 @@
 export const loggerMiddleware = (store) => (next) => (action) => {
-  // console.group(action.type);
-  // console.log("Действие:", action);
-  // console.log("Предыдущее состояние:", store.getState());
+  console.group(action.type);
+  console.log("Действие:", action);
+  console.log("Предыдущее состояние:", store.getState());
 
   const result = next(action);
 
-  // console.log("Новое состояние:", store.getState());
-  // console.groupEnd();
+  console.log("Новое состояние:", store.getState());
+  console.groupEnd();
 
   return result;
 };
